4cc69844d67e78e1dae264dc25be886977a5c907
jllopes <jlglopes96@gmail.com>
2018-09-11 16:20:42 +0100

    Add doc generation script

---
+++ scripts/generate.sh
@@ -0,0 +1,7 @@
+mkdir -p .tmp
+version=`cat .hadolint-version`
+curl -sSL https://raw.githubusercontent.com/hadolint/hadolint/v${version}/README.md -o .tmp/Hadolint.md
+mkdir -p codacy-hadolint/docs
+
+cd docs-generator && \
+sbt "run ../.tmp/Hadolint.md ../codacy-hadolint/docs"
